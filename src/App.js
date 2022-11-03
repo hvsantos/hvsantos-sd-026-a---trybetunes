21dc0ddecff24c2ed1d2f6be2711690ea5d6b92c
Henrique Vilela <hv_santos@outlook.com>
2022-11-03 15:58:38 -0300

    Req 2

--- src/App.js
+++ src/App.js
@@ -8,9 +8,37 @@ import Profile from './pages/Profile';
 import ProfileEdit from './pages/ProfileEdit';
 import Login from './pages/Login';
 import NotFound from './pages/NotFound';
+import { createUser } from './services/userAPI';
 
 class App extends React.Component {
+  state = {
+    userName: '',
+    buttonEnabled: false,
+    shouldRedirect: false,
+    loading: false,
+  };
+
+  enableRedirect = () => {
+    this.setState({ shouldRedirect: false });
+  };
+
+  inputLogin = (event) => {
+    const tres = 3;
+    this.setState({
+      userName: event.target.value,
+      buttonEnabled: event.target.value.length >= tres,
+    });
+  };
+
+  handleClick = async () => {
+    const { userName } = this.state;
+    this.setState({ loading: true });
+    await createUser({ name: userName });
+    this.setState({ shouldRedirect: true }, this.enableRedirect);
+  };
+
   render() {
+    const { buttonEnabled, userName, loading, shouldRedirect } = this.state;
     return (
       <BrowserRouter>
         <Switch>
@@ -19,7 +47,16 @@ class App extends React.Component {
           <Route path="/favorites" component={ Favorites } />
           <Route exact path="/profile" component={ Profile } />
           <Route path="/profile/edit" component={ ProfileEdit } />
-          <Route exact path="/" component={ Login } />
+          <Route exact path="/">
+            <Login
+              buttonEnabled={ buttonEnabled }
+              handleClick={ this.handleClick }
+              inputLogin={ this.inputLogin }
+              shouldRedirect={ shouldRedirect }
+              userName={ userName }
+              loading={ loading }
+            />
+          </Route>
           <Route path="/" component={ NotFound } />
         </Switch>
       </BrowserRouter>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,48 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
+
+import Loading from './Loading';
 
 export default class Login extends Component {
   render() {
+    const { handleClick, inputLogin, buttonEnabled, userName, loading,
+      shouldRedirect } = this.props;
+    if (shouldRedirect) {
+      return (<Redirect to="/search" />);
+    }
     return (
       <div data-testid="page-login">
-        Login
+        { loading
+          ? <Loading />
+          : (
+            <div>
+              <h1>Login</h1>
+              <input
+                type="text"
+                data-testid="login-name-input"
+                onChange={ inputLogin }
+                value={ userName }
+              />
+              <button
+                type="button"
+                data-testid="login-submit-button"
+                onClick={ handleClick }
+                disabled={ !buttonEnabled }
+              >
+                Entrar
+              </button>
+            </div>
+          )}
       </div>
     );
   }
 }
+
+Login.propTypes = {
+  handleClick: PropTypes.func,
+  inputLogin: PropTypes.func,
+  userName: PropTypes.string,
+  buttonEnabled: PropTypes.boolean,
+  loading: PropTypes.string,
+}.isRequired;
