cbefbf80800486fb385f8976bde769b3c5cb23ce
Henrique Vilela <hv_santos@outlook.com>
2022-11-03 16:47:16 -0300

    Req 5 e contando

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,36 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 export default class Search extends Component {
+  state = {
+    searchParam: '',
+    buttonDisabled: true,
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      searchParam: event.target.value,
+      buttonDisabled: (event.target.value.length < 2),
+    });
+  };
+
   render() {
+    const { searchParam, buttonDisabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        Search
+        <input
+          type="text"
+          data-testid="search-artist-input"
+          value={ searchParam }
+          onChange={ this.handleChange }
+        />
+        <button
+          type="button"
+          data-testid="search-artist-button"
+          disabled={ buttonDisabled }
+        >
+          Procurar
+        </button>
       </div>
     );
   }
