e8669b27de530f26b45767adbe8b08d9ab63aa8e
Henrique Vilela <hv_santos@outlook.com>
2022-11-04 19:58:35 -0300

    Req 6

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ class App extends React.Component {
       <BrowserRouter>
         <Switch>
           <Route path="/search" component={ Search } />
-          <Route path="/album/:id" component={ Album } />
+          <Route path="/album/:id" render={ (props) => <Album { ...props } /> } />
           <Route path="/favorites" component={ Favorites } />
           <Route exact path="/profile" component={ Profile } />
           <Route path="/profile/edit" component={ ProfileEdit } />
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Loading from './Loading';
 
 export default class Search extends Component {
   state = {
     searchParam: '',
+    artist: '',
+    resultAlbums: [],
     buttonDisabled: true,
+    loading: false,
   };
 
   handleChange = (event) => {
@@ -14,24 +20,72 @@ export default class Search extends Component {
     });
   };
 
+  searchClick = async () => {
+    const { searchParam } = this.state;
+    this.setState(
+      {
+        loading: true,
+        artist: searchParam,
+      },
+    );
+    const result = await searchAlbumsAPI(searchParam);
+    this.setState({
+      loading: false,
+      resultAlbums: result,
+      searchParam: '',
+    });
+  };
+
   render() {
-    const { searchParam, buttonDisabled } = this.state;
+    const { searchParam, buttonDisabled, resultAlbums,
+      hideSearchInput, loading, artist } = this.state;
+    const divShowResults = (
+      <div>
+        <p>
+          { `Resultado de álbuns de: ${artist}` }
+        </p>
+        {resultAlbums.map((obj) => {
+          const { artistId, artworkUrl100, collectionName,
+            artistName, collectionId } = obj;
+          return (
+            <div key={ `${artistId}-${collectionName}` }>
+              <img src={ artworkUrl100 } alt={ `${collectionName} img` } />
+              <h2>{ collectionName }</h2>
+              <h3>{ artistName }</h3>
+              <Link
+                to={ `/album/${collectionId}` }
+                data-testid={ `link-to-album-${collectionId}` }
+              >
+                Ir para Album
+              </Link>
+            </div>
+          );
+        })}
+      </div>
+    );
+    const didItFind = (resultAlbums.length === 0
+      ? <p>Nenhum álbum foi encontrado</p> : divShowResults);
     return (
       <div data-testid="page-search">
         <Header />
-        <input
-          type="text"
-          data-testid="search-artist-input"
-          value={ searchParam }
-          onChange={ this.handleChange }
-        />
-        <button
-          type="button"
-          data-testid="search-artist-button"
-          disabled={ buttonDisabled }
-        >
-          Procurar
-        </button>
+        <div>
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            value={ searchParam }
+            onChange={ this.handleChange }
+          />
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            onClick={ this.searchClick }
+            disabled={ buttonDisabled }
+          >
+            Procurar
+          </button>
+        </div>
+        { loading && <Loading /> }
+        { (!loading && !hideSearchInput) && didItFind }
       </div>
     );
   }
